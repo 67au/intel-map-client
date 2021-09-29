abc6caf1fcba7f8831f6227b82e726314de77cd7
Eyan <oyy6745@gmail.com>
2021-09-30 01:05:35 +0800

    refactor(client): 修改 AsyncClient 类的 UA.

--- IntelMapClient/client.py
+++ IntelMapClient/client.py
@@ -25,7 +25,8 @@ class AsyncClient:
         'content-type': 'application/json; charset=UTF-8',
         'origin': 'https://intel.ingress.com',
         'referer': 'https://intel.ingress.com/intel',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/93.0.4577.82 Safari/537.36',
     }
     BASE_URL = 'https://intel.ingress.com'
 
