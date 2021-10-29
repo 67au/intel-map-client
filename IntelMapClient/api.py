21610f4a9214dc421d8f0bec894752c0a062d703
Eyan <oyy6745@gmail.com>
2021-10-29 15:28:03 +0800

    pref(api): 更改 api 方法的调用方式

--- IntelMapClient/api.py
+++ IntelMapClient/api.py
@@ -9,14 +9,15 @@ from .errors import ParserError, RequestError, ResultError
 from .types import TileContainer, Tile, Portal, Link, Field, Plext, Reward, Player
 from .utils import MapTiles, datetime2timestamp_ms
 
+logger = logging.getLogger(__name__)
+
 
 class AsyncAPI:
 
-    logger = logging.getLogger(__name__)
+    def __init__(self, client: 'AsyncClient'):
+        self.client = client
 
-    @classmethod
-    async def updatePortals(cls,
-                            client: 'AsyncClient',
+    async def updatePortals(self,
                             portals: Iterator['Portal'],
                             ) -> AsyncIterator['Portal']:
         lock = asyncio.Lock()
@@ -26,32 +27,27 @@ class AsyncAPI:
                 await asyncio.sleep(0)
                 return await coro
 
-        tasks = [asyncio.create_task(with_lock(cls.getPortalByGUID(client, p.guid))) for p in portals]
+        tasks = [asyncio.create_task(with_lock(self.getPortalByGUID(p.guid))) for p in portals]
         for task in asyncio.as_completed(tasks):
             yield await task
 
-
-    @classmethod
-    async def redeemReward(cls,
-                           client: 'AsyncClient',
+    async def redeemReward(self,
                            passcode: 'str') -> Tuple[bool, Union[Tuple['Reward', 'Player'], str]]:
-        result = await client.redeemReward(passcode)
+        result = await self.client.redeemReward(passcode)
         if 'error' in result:
-            cls.logger.error(f"兑换物资失败: {result['error']}")
+            logger.error(f"兑换物资失败: {result['error']}")
             return False, result['error']
         else:
             return True, (Reward(result['rewards']), Player(result['playerData']))
 
-    @classmethod
-    async def sendMessageToComm(cls,
-                                client: 'AsyncClient',
+    async def sendMessageToComm(self,
                                 lat: float,
                                 lng: float,
                                 message: str,
                                 tab: 'str' = 'faction') -> bool:
         if tab not in {'all', 'faction'}:
             raise ParserError(f'"{tab}" not in ["all", "faction"]')
-        result = await client.sendPlext(
+        result = await self.client.sendPlext(
             latE6=int(lat * 1e6),
             lngE6=int(lng * 1e6),
             message=message,
@@ -59,28 +55,23 @@ class AsyncAPI:
         )
         return result == 'success'
 
-    @classmethod
-    async def getPortalByGUID(cls,
-                              client: 'AsyncClient',
+    async def getPortalByGUID(self,
                               guid: 'str',
                               ) -> 'Portal':
-
         max_retries = 5
         for _ in iter(lambda: max_retries > 0, False):
             try:
-                result = await client.getPortalDetails(guid=guid)
-                return cls.parseGameEntities(guid, -1, result)
+                result = await self.client.getPortalDetails(guid=guid)
+                return self.parseGameEntities(guid, -1, result)
             except RequestError:
                 max_retries -= 1
                 t = random.randint(2, 4)
-                cls.logger.warning(f'Portal({guid}) 请求失败，{t}秒后重试')
+                logger.warning(f'Portal({guid}) 请求失败，{t}秒后重试')
                 await asyncio.sleep(t)
-        cls.logger.error(f'无法获取 Portal({guid}) 数据')
+        logger.error(f'无法获取 Portal({guid}) 数据')
         raise ResultError
 
-    @classmethod
-    async def getEntitiesByTiles(cls,
-                                 client: 'AsyncClient',
+    async def getEntitiesByTiles(self,
                                  map_tiles: 'MapTiles',
                                  max_retries: int = 10,
                                  ) -> TileContainer:
@@ -89,24 +80,22 @@ class AsyncAPI:
         todo = map_tiles.tileKeys()
         retries = max_retries
         for _ in iter(lambda: any(todo) and retries > 0, False):
-            tasks = [asyncio.create_task(client.getEntities(todo[k:k + 5])) for k in range(0, len(todo), 5)]
+            tasks = [asyncio.create_task(self.client.getEntities(todo[k:k + 5])) for k in range(0, len(todo), 5)]
             todo = []
             retries -= 1
             for task in asyncio.as_completed(tasks):
                 for k, v in (await task)['map'].items():
                     if 'gameEntities' in v:
-                        entities = [cls.parseGameEntities(*i) for i in v['gameEntities']]
+                        entities = [self.parseGameEntities(*i) for i in v['gameEntities']]
                         container.add(Tile(name=k, gameEntities=entities))
                     else:
                         todo.append(k)
         if any(todo):
-            cls.logger.error(f'无法获取 {map_tiles} 中全部 Tile 的数据')
+            logger.error(f'无法获取 {map_tiles} 中全部 Tile 的数据')
             raise ResultError
         return container
 
-    @classmethod
-    async def getPlextsByTiles(cls,
-                               client: 'AsyncClient',
+    async def getPlextsByTiles(self,
                                map_tiles: 'MapTiles',
                                start: Union['datetime', int] = -1,
                                end: Union['datetime', int] = -1,
@@ -117,7 +106,7 @@ class AsyncAPI:
             raise ParserError(f'"{tab}" not in ["all", "faction", "alerts"]')
         minTimestampMs = datetime2timestamp_ms(start) if isinstance(start, datetime) else start
         maxTimestampMs = datetime2timestamp_ms(end) if isinstance(end, datetime) else end
-        plext = await client.getPlexts(
+        plext = await self.client.getPlexts(
             minLatE6=map_tiles.minLatE6,
             maxLatE6=map_tiles.maxLatE6,
             minLngE6=map_tiles.minLngE6,
@@ -136,7 +125,7 @@ class AsyncAPI:
                 minTimestampMs = plext[-1][1]
             else:
                 maxTimestampMs = plext[-1][1]
-            plext = await client.getPlexts(
+            plext = await self.client.getPlexts(
                 minLatE6=map_tiles.minLatE6,
                 maxLatE6=map_tiles.maxLatE6,
                 minLngE6=map_tiles.minLngE6,
@@ -148,15 +137,13 @@ class AsyncAPI:
                 ascendingTimestampOrder=reverse,
             )
 
-    parser = {
-        'p': Portal,
-        'e': Link,
-        'r': Field,
-    }
-
-    @classmethod
-    def parseGameEntities(cls, guid: str, timestampMs: int, a: list) -> Union['Portal', 'Link', 'Field']:
+    def parseGameEntities(self, guid: str, timestampMs: int, a: list) -> Union['Portal', 'Link', 'Field']:
+        parser = {
+            'p': Portal,
+            'e': Link,
+            'r': Field,
+        }
         try:
-            return cls.parser[a[0]].parse(guid, timestampMs, a)
+            return parser[a[0]].parse(guid, timestampMs, a)
         except KeyError:
             raise ParserError(f'无法解析: {a}')
